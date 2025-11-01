d431005d70d3208a72d47297da89cfe0dc2d5965
Thomas McKernan <tmeaglei@gmail.com>
2025-11-01 17:42:08 -0500

    dont log the env

--- lib/shared/environment.ts
+++ lib/shared/environment.ts
@@ -3,9 +3,7 @@ import {env} from "node:process"
 
 function throwIfEnvNotDefined(key: string): string {
   const value = env[key]
-  console.log(`Value for ${key} is "${value}"`)
   if (value === undefined) {
-    console.error(`Env: ${JSON.stringify(env)}`)
     throw new Error(`Key ${key} is not defined in environment`)
   }
   return value;
